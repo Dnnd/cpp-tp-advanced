c94ee08cd055280c5e5598139fc2fd642986ceb8
Dnnd <d.v.maslennikov@outlook.com>
2020-03-19 01:32:43 +0300

    add argc check, add main return value, switch to vector::data()

--- examples/process-echo/main.cpp
+++ examples/process-echo/main.cpp
@@ -1,17 +1,24 @@
 #include "process/process.hpp"
 #include <algorithm>
 #include <cassert>
+#include <iostream>
 #include <vector>
 
 int main(int argc, char **argv) {
-  std::vector<char> sendbuf(1024 * 16, 'a');
-  std::vector<char> recvbuf(1024 * 16, '\0');
+  if (argc < 2) {
+    std::cerr << "usage: process-echo <path_to_executable>\n";
+    return EXIT_FAILURE;
+  }
+  constexpr std::size_t size_16kb = 1024 * 16;
+
+  std::vector<char> sendbuf(size_16kb, 'a');
+  std::vector<char> recvbuf(size_16kb, '\0');
   for (int i = 0; i < 10000; ++i) {
     Process p{argv[1]};
     assert(p.isReadable());
     p.writeExact(sendbuf.data(), sendbuf.size());
     p.closeStdin();
-    p.readExact(&recvbuf[0], sendbuf.size());
+    p.readExact(recvbuf.data(), sendbuf.size());
     assert(recvbuf == sendbuf);
     std::transform(recvbuf.begin(), recvbuf.end(), recvbuf.begin(),
                    [](auto a) { return '\0'; });
@@ -21,4 +28,5 @@ int main(int argc, char **argv) {
     p.close();
     assert(!p.isReadable());
   }
+  return 0;
 }
