1421cfb687bb45302ce3de99711e9cb3d79aec0f
Dnnd <d.v.maslennikov@outlook.com>
2020-03-21 13:41:12 +0300

    reduce the number of test process launches

--- examples/process-echo/main.cpp
+++ examples/process-echo/main.cpp
@@ -1,5 +1,4 @@
 #include "process/process.hpp"
-#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -9,11 +8,11 @@ int main(int argc, char **argv) {
     std::cerr << "usage: process-echo <path_to_executable>\n";
     return EXIT_FAILURE;
   }
-  constexpr std::size_t size_16kb = 1024 * 16;
+  constexpr std::size_t test_buff_size = 1024 * 16;
 
-  std::vector<char> sendbuf(size_16kb, 'a');
-  std::vector<char> recvbuf(size_16kb, '\0');
-  for (int i = 0; i < 10000; ++i) {
+  std::vector<char> sendbuf(test_buff_size, 'a');
+  std::vector<char> recvbuf(test_buff_size, '\0');
+  for (int i = 0; i < 100; ++i) {
     Process p{argv[1]};
     assert(p.isReadable());
     p.writeExact(sendbuf.data(), sendbuf.size());
