d125e1debea6d3c48b9cf2e3557affb468f1898e
Dnnd <d.v.maslennikov@outlook.com>
2020-04-17 14:27:49 +0300

    add custom exceptions

---
+++ include/shmem/exceptions.hpp
@@ -0,0 +1,33 @@
+#ifndef PROCESS_WRAPPER_INCLUDE_SHMEM_EXCEPTIONS_HPP_
+#define PROCESS_WRAPPER_INCLUDE_SHMEM_EXCEPTIONS_HPP_
+
+#include <exception>
+#include <string>
+
+namespace shmem {
+class Exception : public std::exception {
+public:
+  explicit Exception(const std::string &message);
+  [[nodiscard]] const char *what() const noexcept override;
+
+private:
+  std::string message_;
+};
+
+class ErrnoException : public Exception {
+public:
+  ErrnoException(const std::string &message, int errno_val);
+  ;
+  [[nodiscard]] int getErrno() const noexcept;
+
+private:
+  int errno_val_;
+};
+
+class MutexException : public ErrnoException {
+public:
+  MutexException(const std::string &message, int errno_val);
+  ;
+};
+} // namespace shmem
+#endif // PROCESS_WRAPPER_INCLUDE_SHMEM_EXCEPTIONS_HPP_
--- lib/shmem/shmutex.cpp
+++ lib/shmem/shmutex.cpp
@@ -1,20 +1,21 @@
 #include "shmem/shmutex.hpp"
+#include "shmem/exceptions.hpp"
 namespace shmem {
 ShMutex::ShMutex(SharedRegionView &region) {
   sem_t *sem = shmnew<sem_t>(region);
   if (-1 == sem_init(sem, 1, 1)) {
-    throw std::runtime_error("unableto construct shmutex");
+    throw MutexException("unable to construct shmutex", errno);
   };
   sem_ = sem;
 }
 void ShMutex::lock() {
   if (-1 == sem_wait(sem_)) {
-    throw std::runtime_error("unable to lock");
+    throw MutexException("unable to lock", errno);
   };
 }
 void ShMutex::unlock() {
   if (-1 == sem_post(sem_)) {
-    throw std::runtime_error("unable to unlock");
+    throw MutexException("unable to unlock", errno);
   };
 }
 ShMutex::~ShMutex() noexcept { sem_destroy(sem_); }
