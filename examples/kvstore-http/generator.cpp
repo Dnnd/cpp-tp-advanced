7e6f59728ca648af61b6c68ebbfa6edf791dcc07
Dnnd <d.v.maslennikov@outlook.com>
2020-05-29 03:56:17 +0300

    add kvstore example

---
+++ examples/kvstore-http/generator.cpp
@@ -0,0 +1,47 @@
+#include "data.hpp"
+#include "generator_cli.hpp"
+#include "kvstore/descriptor.hpp"
+#include "kvstore/exceptions.hpp"
+#include "kvstore/file.hpp"
+#include <cstring>
+#include <filesystem>
+#include <sys/fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <vector>
+
+File create_and_map(std::filesystem::path dest, size_t size) {
+  Descriptor fd{open(dest.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0600)};
+  if (fd.get() == -1) {
+    throw ErrnoException("unable to create new file or truncate existing file", errno);
+  }
+  lseek(fd.get(), size + 1, SEEK_SET);
+  write(fd.get(), "", 1);
+  return {dest, size, File::Mode::READWRITE};
+}
+
+void fill_data_page(std::vector<Data> &page, Key key_begin, std::size_t step) {
+  for (std::size_t i = 0; i < page.size(); ++i) {
+    Key new_key = key_begin + i * step;
+    page[i].key = new_key;
+    uint8_t *data = page[i].data;
+    std::fill(data, data + sizeof(Data::data), 'x');
+  }
+}
+
+void generate_data(std::filesystem::path dest, size_t keys, size_t step) {
+  size_t chunk_size = (4096 < keys) ? 4096 : keys;
+  File data = create_and_map(dest, keys * sizeof(Data));
+  std::vector<Data> data_page(chunk_size);
+  for (size_t i = 0; i < keys; i += chunk_size) {
+    fill_data_page(data_page, i * step, step);
+    for (size_t j = 0; j < data_page.size(); ++j) {
+      data.write_at<Data>(data_page[j], (i + j) * sizeof(Data));
+    }
+  }
+}
+
+int main(int argc, char **argv) {
+  GeneratorConfig cfg = parse_cli_opts(argc, argv);
+  generate_data(cfg.filename, cfg.keys_number, cfg.keys_step);
+}
---
+++ examples/kvstore-http/include/generator_cli.hpp
@@ -0,0 +1,41 @@
+#ifndef PROCESS_WRAPPER_EXAMPLES_HTTP_EXAMPLE_CLI_HPP_
+#define PROCESS_WRAPPER_EXAMPLES_HTTP_EXAMPLE_CLI_HPP_
+#include "log/level.hpp"
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <getopt.h>
+#include <optional>
+#include <string>
+
+struct GeneratorConfig {
+  std::string filename{"/tmp/data.db"};
+  std::size_t keys_number{65536};
+  std::size_t keys_step{100};
+};
+
+GeneratorConfig parse_cli_opts(int argc, char *const argv[]) {
+  GeneratorConfig parsed_config{};
+  while (true) {
+    int opt = getopt(argc, argv, "f:n:s");
+    if (opt == -1) {
+      break;
+    }
+    switch (opt) {
+    case 'n':
+      parsed_config.keys_number = std::stoull(optarg);
+      break;
+    case 's':
+      parsed_config.keys_step = std::stoull(optarg);
+      break;
+    case 'f':
+      parsed_config.filename = optarg;
+      break;
+    case '?':
+    default:
+      break;
+    }
+  }
+  return parsed_config;
+}
+#endif // PROCESS_WRAPPER_EXAMPLES_HTTP_EXAMPLE_CLI_HPP_
